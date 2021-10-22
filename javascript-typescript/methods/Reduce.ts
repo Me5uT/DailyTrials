c38cb7a198138835bbe25cbb581566f8e218111c
Me5uT <me5ut@windowslive.com>
2021-10-22 22:46:42 +0300

    Methods: Added Reduce Example

---
+++ javascript-typescript/methods/Reduce.ts
@@ -0,0 +1,47 @@
+interface IShoppingCart {
+  item: string;
+  price: number;
+  quantity: number;
+  total: () => number;
+}
+
+const myCart: IShoppingCart[] = [
+  {
+    item: "Egg",
+    price: 1,
+    quantity: 15,
+    total: function () {
+      return this.price * this.quantity;
+    },
+  },
+  {
+    item: "Bread",
+    price: 3,
+    quantity: 1,
+    total: function () {
+      return this.price * this.quantity;
+    },
+  },
+  {
+    item: "Milk",
+    price: 5,
+    quantity: 1,
+    total: function () {
+      return this.price * this.quantity;
+    },
+  },
+  {
+    item: "chocolate",
+    price: 5,
+    quantity: 2,
+    total: function () {
+      return this.price * this.quantity;
+    },
+  },
+];
+
+const totalShoppingFee = myCart
+  .map((product) => product.total())
+  .reduce((totalPrice, productPrice) => totalPrice + productPrice);
+
+console.log(totalShoppingFee);
