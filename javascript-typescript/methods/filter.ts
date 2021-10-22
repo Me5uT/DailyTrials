8e5607b8dae861db68ef6609d446d1d0fd7208f8
Me5uT <me5ut@windowslive.com>
2021-10-22 22:04:40 +0300

    Methods: Added Filter Method Example

---
+++ javascript-typescript/methods/filter.ts
@@ -0,0 +1,26 @@
+interface IPeople {
+  name: string;
+  age: number;
+}
+
+const people: IPeople[] = [
+  {
+    name: "Ahmet",
+    age: 17,
+  },
+  {
+    name: "Ayşe",
+    age: 19,
+  },
+  {
+    name: "Mehmet",
+    age: 16,
+  },
+  {
+    name: "Ali",
+    age: 20,
+  },
+];
+
+const adultPeople: IPeople[] = people.filter((human) => human.age > 18);
+console.log("checkAdult", adultPeople);
