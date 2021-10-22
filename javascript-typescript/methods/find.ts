6a4eb7770efd0f37470489218738ab957d56e3c6
Me5uT <me5ut@windowslive.com>
2021-10-22 21:46:18 +0300

    Methods: Added Find Method example

---
+++ javascript-typescript/methods/find.ts
@@ -0,0 +1,39 @@
+interface IUser {
+  id: number;
+  name: string;
+}
+
+const userArr: IUser[] = [
+  {
+    id: 1,
+    name: "Mesut",
+  },
+  {
+    id: 2,
+    name: "Emin",
+  },
+  {
+    id: 3,
+    name: "Furkan",
+  },
+  {
+    id: 4,
+    name: "Mervan",
+  },
+  {
+    id: 5,
+    name: "Gönenç",
+  },
+];
+
+const findUserNameById = (userId: number): string => {
+  const matchedUser = userArr.find((user: IUser) => user.id == userId);
+
+  if (matchedUser != undefined) {
+    return `${matchedUser.name}`;
+  } else {
+    return "Not Found User";
+  }
+};
+
+findUserNameById(5);
