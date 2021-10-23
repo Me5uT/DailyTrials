2ae836ead767c5b35e764f084fd947efe4216fc7
Me5uT <me5ut@windowslive.com>
2021-10-24 01:03:37 +0300

    Methods: Added Map Method Example

---
+++ javascript-typescript/methods/Map.ts
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const squareOfNumbers = numbers.map((number: number) => number * number);
+console.log("squareOfNumbers", squareOfNumbers); // output [ 1, 4, 9, 16, 25, 36 ]
