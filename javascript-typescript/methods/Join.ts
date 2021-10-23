e13c6e6db4477f77001838592fdb3d6349308104
Me5uT <me5ut@windowslive.com>
2021-10-23 23:08:08 +0300

    Methods: Added Join Method Example

---
+++ javascript-typescript/methods/Join.ts
@@ -0,0 +1,10 @@
+const arr = ["Elma", "Muz", "Armut", "Çilek"];
+
+const joinExampleOne = arr.join("");
+console.log(joinExampleOne);
+
+const joinExampleTwo = arr.join(" * ");
+console.log(joinExampleTwo);
+
+const joinExampleThree = arr.join(".");
+console.log(joinExampleThree);
