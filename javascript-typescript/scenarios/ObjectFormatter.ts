2c3fb4a941bf37c0f3b883d043fbf54f0376d7b7
Me5uT <me5ut@windowslive.com>
2021-11-01 15:42:44 +0300

    Scenarios: Added ObjectFormatter Examples

---
+++ javascript-typescript/scenarios/ObjectFormatter.ts
@@ -0,0 +1,29 @@
+const objectFormatter = JSON.stringify(
+  {
+    name: "mesut",
+    surName: "caga",
+    link: "www.google.com.tr",
+  },
+  null,
+  1 // boşluk sayısı
+);
+
+// console.log(objectFormatter);
+
+const objectFormatter2 = (obje: Object | any[]) => {
+  const replacer = null;
+  const space: number = 4;
+  const formatter = JSON.stringify(obje, replacer, space);
+
+  return console.log(formatter);
+};
+
+const objec = [
+  {
+    name: "Ahmet",
+    point: 1,
+    createDate: "2021-11-01T12:32:51.211Z",
+  },
+];
+
+objectFormatter2(objec);
